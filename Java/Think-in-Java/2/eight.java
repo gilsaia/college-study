f3c73865c52a09930930d40d94ab9153b4439b31
gilsaia <794433219@qq.com>
2018-09-04 23:16:55 +0800

    change

--- Java/Think-in-Java/2/eight.java
+++ Java/Think-in-Java/2/eight.java
@@ -11,22 +11,22 @@ public class eight {
 		System.out.println("StaticTest.i= " + StaticTest.i);
 		StaticTest st1 = new StaticTest();
 		StaticTest st2 = new StaticTest();
-		System.out.println("st1.i= " + st1.i);
-		System.out.println("st2.i= " + st2.i);
+		System.out.println("st1.i= " + i);
+		System.out.println("st2.i= " + i);
 		Incrementable.increment();
 		System.out.println("After Incrementable.increment() called: ");
-		System.out.println("st1.i = " + st1.i);
-		System.out.println("st2.i = " + st2.i);
+		System.out.println("st1.i = " + i);
+		System.out.println("st2.i = " + i);
 		Incrementable.increment();
 		System.out.println("After Incrementable.increment called: ");
-		System.out.println("st1.i = " + st1.i);
-		System.out.println("st2.i = " + st2.i);
-		st1.i = 3;
+		System.out.println("st1.i = " + i);
+		System.out.println("st2.i = " + i);
+		i = 3;
 		System.out.println("After st1.i = 3, ");
-		System.out.println("st1.i = " + st1.i);
-		System.out.println("st2.i = " + st2.i);
+		System.out.println("st1.i = " + i);
+		System.out.println("st2.i = " + i);
 		System.out.println("Create another StaticTest, st3.");
 		StaticTest st3 = new StaticTest();
-		System.out.println("st3.i = " + st3.i);
+		System.out.println("st3.i = " + i);
 	}
 }
