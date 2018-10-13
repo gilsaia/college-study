b48577815be617c1d314c913ce6633866c974324
gilsaia <794433219@qq.com>
2018-10-13 13:47:53 +0800

    9.2

---
+++ Java/Think-in-Java/9/five.java
@@ -0,0 +1,12 @@
+interface basic
+{
+    void play();
+    void tune();
+    void adjust();
+}
+class totry implements basic
+{
+    public void play(){}
+    public void tune(){}
+    public void adjust(){}
+}
