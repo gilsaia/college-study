37d9fad3bc73d03b838f571c8cc90cffacb2976f
gilsaia <794433219@qq.com>
2018-10-19 23:20:16 +0800

    homework

--- c++/homework/buddysystem.cpp
+++ c++/homework/buddysystem.cpp
@@ -1,3 +1,9 @@
+//开始数字代表内存总大小
+//之后第一个数字代表操作类型
+//0为索取空间，之后为空间对应标号和空间大小
+//1为归还空间，之后为空间对应标号
+//2为显示当前剩余空间状态
+//-1为结束程序
 #ifndef BUDDYSYSTEM_H
 #define BUDDYSYSTEM_H
 #include<iostream>
@@ -6,22 +12,31 @@
 #include<cstring>
 #include<vector>
 #include<utility>
+#include<map>
+#include<iomanip>
 using ll=long long;
 using std::pair;
+using std::make_pair;
 using std::cout;
 using std::cin;
+using std::ostream;
 using std::fstream;
 using std::ifstream;
 using std::ofstream;
 using std::vector;
+using std::map;
+using std::endl;
+using std::setw;
+using std::left;
+using std::right;
 struct node
 {
     ll address,size;
     node *next;
-    node(ll add=0,ll size=1024,node *n=nullptr):address(add),next(n)
+    node(ll add=0,ll sizet=1024,node *n=nullptr):address(add),next(n)
     {
         ll modsize=1;
-        while(modsize<size)
+        while(modsize<sizet)
         {
             modsize<<=1;
         }
@@ -39,13 +54,103 @@ struct node
 class buddysystem
 {
     public:
-    buddysystem()
+    void print(ostream& output)
+    {
+        output<<left<<setw(12)<<"address"<<setw(12)<<"size"<<endl<<right;
+        if(head==nullptr)
+        {
+            return;
+        }
+        node *curr=head;
+        output<<left<<setw(12)<<curr->address<<setw(12)<<curr->size<<endl<<right;
+        while(curr!=tail)
+        {
+            curr=curr->next;            
+            output<<left<<setw(12)<<curr->address<<setw(12)<<curr->size<<endl<<right;
+        }
+    }
+    buddysystem(ostream& wrong=cout):wrongout(wrong)
     {
         tail=head=new node();
     }
-    buddysystem(ll size)
+    buddysystem(ll size,ostream& wrong=cout):wrongout(wrong)
     {
         tail=head=new node(0,size);
+    }
+    bool request(ll num,ll size)
+    {
+        if(used.find(num)!=used.end())
+        {
+            wrongout<<"wrong request!"<<endl;
+            return false;
+        }
+        ll sizet=1;
+        while(sizet<size)
+        {
+            sizet<<=1;
+        }
+        node *curr=head;
+        while(curr!=tail&&curr->next->size>=sizet)
+        {
+            curr=curr->next;
+        }
+        if(curr->size==sizet)
+        {
+            used[num]=make_pair(curr->address,sizet);
+            erase(curr->address,curr->size);
+            return true;
+        }
+        if(curr->size<sizet)
+        {
+            wrongout<<"memory limit exceed!"<<endl;
+            return false;
+        }
+        ll addresstmp=curr->address,sizetmp=curr->size;
+        while(sizetmp>sizet)
+        {
+            erase(addresstmp,sizetmp);
+            insert(addresstmp,sizetmp/2);
+            curr=insert(addresstmp+sizetmp/2,sizetmp/2);
+            addresstmp=addresstmp+sizetmp/2;
+            sizetmp/=2;
+        }
+        used[num]=make_pair(addresstmp,sizetmp);
+        erase(addresstmp,sizetmp);
+        return true;
+    }
+    bool realease(ll num)
+    {
+        if(used.find(num)==used.end())
+        {
+            wrongout<<"doesn't exist!"<<endl;
+            return false;
+        }
+        ll addressrea=used[num].first,sizerea=used[num].second;
+        ll addresstmp=0,sizetmp=0;
+        used.erase(num);
+        node *curr=insert(addressrea,sizerea),*find=head;
+        while(find!=curr)
+        {
+            if(curr->next!=nullptr&&curr->next->size==curr->size&&curr->next->address==curr->address+curr->size)
+            {
+                addresstmp=curr->address,sizetmp=curr->size;
+                erase(addresstmp,sizetmp);
+                erase(addresstmp+sizetmp,sizetmp);
+                curr=insert(addresstmp,sizetmp*2);
+                continue;
+            }
+            if(find->next==curr&&find->size==curr->size&&find->address+find->size==curr->address)
+            {
+                addresstmp=find->address,sizetmp=find->size;
+                erase(addresstmp,sizetmp);
+                erase(addresstmp+sizetmp,sizetmp);
+                curr=insert(addresstmp,sizetmp*2);
+                find=head;
+                continue;
+            }
+            find=find->next;
+        }
+        return true;
     }
     private:
     node *insert(ll address,ll size)
@@ -122,6 +227,41 @@ class buddysystem
         }
     }
     node *head,*tail;
-    vector<pair<ll,ll>> used;
+    map<ll,pair<ll,ll>> used;
+    ostream& wrongout;
 };
+int main()
+{
+    ll totalsize;
+	ifstream input("G:\\cs\\coding\\c++\\homework\\buddysystemin.txt");
+	ofstream output("G:\\cs\\coding\\c++\\homework\\buddysystemout.txt");
+    input>>totalsize;
+    buddysystem memory(totalsize,output);
+    int operationnum;
+    while(input>>operationnum)
+    {
+        if(operationnum==-1)
+        {
+            break;
+        }
+        ll num,size;
+        switch(operationnum)
+        {
+            case 0:
+            input>>num>>size;
+            memory.request(num,size);
+            break;
+            case 1:
+            input>>num;
+            memory.realease(num);
+            break;
+            case 2:
+            memory.print(output);
+            break;
+            default:
+            break;
+        }
+    }
+    return 0;
+}
 #endif
